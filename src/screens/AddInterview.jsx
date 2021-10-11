03a2b500a2281a51b97f740ace161f7015826526
unknown <adibcse10@gmail.com>
2021-10-11 09:54:24 +0600

    ui chnages and bug fixed latest

--- src/screens/AddInterview.jsx
+++ src/screens/AddInterview.jsx
@@ -8,7 +8,7 @@ import InterviewService from '../services/InterviewService';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import { CKEditor } from '@ckeditor/ckeditor5-react'
 
-// html parser
+//html parser
 //import HtmlParser from 'react-html-parser';
 
 
